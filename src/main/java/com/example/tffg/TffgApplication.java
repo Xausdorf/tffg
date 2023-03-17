c1b5ff828f4c5952c0afb922c5aedc5c16d374d4
Vasilii Parnikov <vasya.yakut.sakha@gmail.com>
2023-03-17 18:42:55 +0300

    Initial commit

---
+++ src/main/java/com/example/tffg/TffgApplication.java
@@ -0,0 +1,13 @@
+package com.example.tffg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TffgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TffgApplication.class, args);
+	}
+
+}
